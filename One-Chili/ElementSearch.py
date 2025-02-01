c93c50fb24e7a0fcae33eabec56ffb16395c69ea
Ray <105521700+Barrow75@users.noreply.github.com>
2025-01-31 22:34:31 -0700

    Update ElementSearch.py

--- One-Chili/ElementSearch.py
+++ One-Chili/ElementSearch.py
@@ -22,3 +22,41 @@ def main():
     else:
         print("True")
 main()
+
+
+# Solution 2
+def OrderList():
+    order = input("Enter Numbers: ").split()
+    new = list(map(int, order))
+    new.sort()
+
+    return new
+
+new = OrderList()
+print(new)
+
+def BinarySearch(new):
+
+    check_list = int(input("Is this number in the list: "))
+    start_index = 0
+    end_index = len(new) - 1
+
+    while True:
+
+        if start_index > end_index:
+            return -1
+
+        half_list = (end_index + start_index) // 2
+
+        if check_list == new[int(half_list)]:
+            return half_list
+
+        elif check_list > new[int(half_list)]:
+            start_index = half_list + 1
+        else:
+            start_index = half_list - 1
+
+
+
+result = BinarySearch(new)
+print(result)
